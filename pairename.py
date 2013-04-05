898a0711e87ef1bbd9907209b83e836339e1082c
dtouch3d completely <dtouch3d@gmail.com>
2013-04-06 01:21:31 +0300

    argparse basics

--- pairename.py
+++ pairename.py
@@ -3,42 +3,56 @@
 import os
 import sys
 import argparse
-"""
-parser = argparse.ArgumentParser(description='Renames files to match respective video file names (Default: srt)')
 
-parser.add_argument('--extension', nargs=1, default='srt', dest=ext)
-parser.add_argument('--matching', nargs=1, default='avi', dest=matching)
-parser.add_argument('--debug', action='store_true', help='dry run, print debug messages')
-"""
+
+parser = argparse.ArgumentParser(description=
+         'Renames files of one extension to match name of the other'
+         'Target folder must hold only two kinds of files.')
+
+parser.add_argument('-m', action="store", dest="matchingExt", help=
+                    'The extension of the filenames you want to rename to')
+parser.add_argument('-r', action="store", dest="toRenameExt", help=
+                    'The extension of the files you want to rename')
+
+print(parser.parse_args())
+
+# parser.add_argument('--debug', action='store_true', help='dry run, print debug messages')
+
 
 # TODO: argparser, verbose ?
-#       Eliminate ext1,2 altogether ?
+#       Eliminate matchingExt,2 altogether ?
+#       Backup / revert changes ?
+#       Auto find extensions
+
 if len(sys.argv) != 4:
-  print("subrename.py <target folder> <extension1> <extension2>")
-  print("Renames files with ext1 with the name of the respective  alphabetical order of ext2")
-  sys.exit(0)
+    print("subrename.py <target folder> <toRenameExt> <matchingExt>\n"
+          "Renames files with matchingExt with the name of the respective "
+          "alphabetical order of toRenameExt")
+    sys.exit(0)
 
 path = sys.argv[1]
-ext1 = sys.argv[2]
-ext2 = sys.argv[3]
+matchingExt = sys.argv[2]   # matching extension
+toRenameExt = sys.argv[3]   # rename files with this extension
 
 pwd = os.path.realpath(path)
 os.chdir(pwd)
 
 ls = os.listdir(pwd)
 
-ext1List = [x for x in ls if ext1 in x]
-ext1List.sort()
+matchingExtList = [x for x in ls if matchingExt in x]
+matchingExtList.sort()
 
-ext2List = [x for x in ls if ext2 in x]
-ext2List.sort()
+toRenameExtList = [x for x in ls if toRenameExt in x]
+toRenameExtList.sort()
 
-print("ext1List: " + str(ext1List))
-print("ext2List: " + str(ext2List))
+print("matchingExtList: ", str(matchingExtList))
+print("toRenameExtList: ", str(toRenameExtList))
 
-ext2ListNew = [os.path.splitext(x)[0] + "." + ext2 for x in ext1List]
-print("ext2ListNew:", ext2ListNew)
+toRenameExtListNew = [os.path.splitext(x)[0] + "." + toRenameExt
+                     for x in matchingExtList]
 
-for ext2, ext2New in zip(ext2List, ext2ListNew):
-  print("Renaming " + ext2 + " to " + ext2New)
-  os.rename(ext2, ext2New)
+print("toRenameExtListNew:", toRenameExtListNew)
+
+for toRenameExt, toRenameExtNew in zip(toRenameExtList, toRenameExtListNew):
+    print("Renaming " + toRenameExt + " to " + toRenameExtNew)
+    os.rename(toRenameExt, toRenameExtNew)
